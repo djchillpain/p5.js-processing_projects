59b99512b04413cb1fe66ae7a485a0b9aa159665
djchillpain <88328645+djchillpain@users.noreply.github.com>
2022-05-31 08:38:46 +0000

    Update sin_function.js

--- p5.js/sin_function.js
+++ p5.js/sin_function.js
@@ -3,17 +3,18 @@ let z = 0, g=100, h, col;
 function setup() {
   createCanvas(400, 400, P2D);
   noFill();
+  strokeWeight(2);
 }
 
 function draw() {
   background(0);
   translate(width/2, height/2);
-  for(let i=-g;i<=g;i+=0.05){
+  for(let i=-g;i<=g;i+=0.1){
     col = map(i, -100, 100, 0, 255);
     stroke(10, col, 10);
     h = sn(i, z);
     point(i, h);
-    z += 0.00001;
+    z += 0.00003;
   }
   
 }
